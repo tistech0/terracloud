62c14263a84fe94cae3e20ac61f76e2532c62d49
Administrator <gitlab_admin_b5a294@example.com>
2024-11-21 14:10:36 +0100

    🐛 update azur secret

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,5 +5,5 @@ provider "azurerm" {
   subscription_id = "1eb5e572-df10-47a3-977e-b0ec272641e4"
   tenant_id       = "901cb4ca-b862-4029-9306-e5cd0f6d9f86"
   client_id       = "5cc4162b-e2d6-4ace-ba68-d8385875f97b"
-  client_secret   = "w3e8Q~ourwQURYU75ssHRTH-mlgWLJ3Mau3hYbeX"
+  client_secret   = "ssb8Q~YkLI5CTgdXpenUwOVMsFtgtmhGMEuAWdiH"
 }
